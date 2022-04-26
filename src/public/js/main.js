2b8e107390fe9f01c506dc014f461190f5c72e76
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-26 14:57:43 -0300

    Script added

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,11 +1,11 @@
 //creating a map template, with coordinates and zoom
-const map = L.map("map-template").setView([-31.416668, -64.183334], 13);
+const map = L.map('map-template').setView([-31.416668, -64.183334], 13);
 
 //initializing connection for socket.io
 const socket = io();
 
 //choosing a map style
-const tileURL = "https://a.tile.openstreetmap.de/{z}/{x}/{y}.png";
+const tileURL = 'https://a.tile.openstreetmap.de/{z}/{x}/{y}.png';
 
 //adding our tile style layer to our map
 L.tileLayer(tileURL, {
@@ -16,22 +16,22 @@ L.tileLayer(tileURL, {
 //allowing user to add his/her own coordinates through asking to know his/her location
 map.locate({ enableHighAccuracy: true });
 //adding listener to locationFound event
-map.on("locationfound", (e) => {
+map.on('locationfound', (e) => {
 	const coords = [e.latlng.lat, e.latlng.lng]; //getting our user coordinates
 	const marker = L.marker(coords); //creating a location marker
-	marker.bindPopup("You are here!"); //adding to our marker a message
+	marker.bindPopup('You are here!'); //adding to our marker a message
 	map.addLayer(marker); //adding user location's marker with message layer to our map
-	socket.emit("userCoordinates", e.latlng); //emiting event
+	socket.emit('userCoordinates', e.latlng); //emiting event
 });
 
 //listening event
-socket.on("newUserCoordinates", (coords) => {
+socket.on('newUserCoordinates', (coords) => {
 	//creating new user's location marker
-	console.log("A new user is connected");
-	const marker = L.marker([coords.lat + 1, coords.lng + 1]); //moving coordinates to see the new marker when testing it myself
+	console.log('A new user is connected', coords);
+	const marker = L.marker([coords.lat, coords.lng]); //moving coordinates to see the new marker when testing it myself
 
 	//adding to new user's marker a message
-	marker.bindPopup("Hello there, new user!");
+	marker.bindPopup('Hello there, new user!');
 
 	//adding new user's marker with message layer to our map
 	map.addLayer(marker);
